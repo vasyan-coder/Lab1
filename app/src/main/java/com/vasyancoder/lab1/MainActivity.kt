61e9878a19a818dcbd25c9b0d5b5cd74bedb075f
Vasyan <vasyan.coder@gmail.com>
2023-02-11 14:35:26 +0300

    feat: add logs and toasts about MainActivity lifecycle

--- app/src/main/java/com/vasyancoder/lab1/MainActivity.kt
+++ app/src/main/java/com/vasyancoder/lab1/MainActivity.kt
@@ -2,10 +2,56 @@ package com.vasyancoder.lab1
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        Log.d(TAG, "onCreate")
+        Toast.makeText(this, "onCreate", Toast.LENGTH_SHORT).show()
     }
+
+    override fun onStart() {
+        super.onStart()
+        Log.d(TAG, "onStart")
+        Toast.makeText(this, "onStart", Toast.LENGTH_SHORT).show()
+    }
+
+    override fun onResume() {
+        super.onResume()
+        Log.d(TAG, "onResume")
+        Toast.makeText(this, "onResume", Toast.LENGTH_SHORT).show()
+    }
+
+    override fun onPause() {
+        super.onPause()
+        Log.d(TAG, "onPause")
+        Toast.makeText(this, "onPause", Toast.LENGTH_SHORT).show()
+    }
+
+    override fun onStop() {
+        super.onStop()
+        Log.d(TAG, "onStop")
+        Toast.makeText(this, "onStop", Toast.LENGTH_SHORT).show()
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        Log.d(TAG, "onDestroy")
+        Toast.makeText(this, "onDestroy", Toast.LENGTH_SHORT).show()
+    }
+
+    override fun onRestart() {
+        super.onRestart()
+        Log.d(TAG, "onRestart")
+        Toast.makeText(this, "onRestart", Toast.LENGTH_SHORT).show()
+    }
+
+    companion object {
+
+        private const val TAG = "MainActivity"
+    }
+
 }
