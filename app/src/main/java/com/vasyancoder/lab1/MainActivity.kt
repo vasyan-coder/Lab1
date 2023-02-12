271894e481f6becc1b09d4045536d26514a07260
Vasyan <vasyan.coder@gmail.com>
2023-02-12 22:06:02 +0300

    feat: add Log.e for Activity lifecycle

--- app/src/main/java/com/vasyancoder/lab1/MainActivity.kt
+++ app/src/main/java/com/vasyancoder/lab1/MainActivity.kt
@@ -10,42 +10,49 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         Log.d(TAG, "onCreate")
+        Log.e(TAG, "onCreate")
         Toast.makeText(this, "onCreate", Toast.LENGTH_SHORT).show()
     }
 
     override fun onStart() {
         super.onStart()
         Log.d(TAG, "onStart")
+        Log.e(TAG, "onStart")
         Toast.makeText(this, "onStart", Toast.LENGTH_SHORT).show()
     }
 
     override fun onResume() {
         super.onResume()
         Log.d(TAG, "onResume")
+        Log.e(TAG, "onResume")
         Toast.makeText(this, "onResume", Toast.LENGTH_SHORT).show()
     }
 
     override fun onPause() {
         super.onPause()
         Log.d(TAG, "onPause")
+        Log.e(TAG, "onPause")
         Toast.makeText(this, "onPause", Toast.LENGTH_SHORT).show()
     }
 
     override fun onStop() {
         super.onStop()
         Log.d(TAG, "onStop")
+        Log.e(TAG, "onStop")
         Toast.makeText(this, "onStop", Toast.LENGTH_SHORT).show()
     }
 
     override fun onDestroy() {
         super.onDestroy()
         Log.d(TAG, "onDestroy")
+        Log.e(TAG, "onDestroy")
         Toast.makeText(this, "onDestroy", Toast.LENGTH_SHORT).show()
     }
 
     override fun onRestart() {
         super.onRestart()
         Log.d(TAG, "onRestart")
+        Log.e(TAG, "onRestart")
         Toast.makeText(this, "onRestart", Toast.LENGTH_SHORT).show()
     }
 
